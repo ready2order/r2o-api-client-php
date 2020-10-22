62af08cfce1588d8a70b1932e08576a1d4fe26e2
Christopher Fuchs <christopher.fuchs@ready2order.com>
2020-10-22 17:07:14 +0200

    Added Client::setLanguage() to set Accept-Language header

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -17,4 +17,39 @@ class ClientTest extends AbstractTestCase
 
         $this->assertSame(5, $client->getTimeout());
     }
+
+    /**
+     * @dataProvider languageDataProvider
+     *
+     * @param mixed $expectedError
+     */
+    public function testSettingLanguage(string $language, $expectedError): void
+    {
+        $client = $this->getApiClient();
+        $client->setLanguage($language);
+        $this->expectExceptionMessage($expectedError);
+        $client->put('products', []);
+    }
+
+    public function languageDataProvider()
+    {
+        return [
+            [
+                'de-AT',
+                <<<'EOT'
+                product name muss ausgefüllt sein.
+                product price muss ausgefüllt sein.
+                product vat muss ausgefüllt sein.
+                EOT
+            ],
+            [
+                'en-US',
+                <<<'EOT'
+                The product name field is required.
+                The product price field is required.
+                The product vat field is required.
+                EOT
+            ],
+        ];
+    }
 }
